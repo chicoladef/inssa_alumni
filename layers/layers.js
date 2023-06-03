a221e3d9556c512eca840d2a7ef0e16bc27dd87f
chicoladef <113385939+chicoladef@users.noreply.github.com>
2023-06-03 02:41:11 +0000

    Add files via upload

--- layers/layers.js
+++ layers/layers.js
@@ -10,27 +10,27 @@ var wms_layers = [];
     attributions: [new ol.Attribution({html: '<a href="https://www.openstreetmap.org/copyright">© OpenStreetMap contributors, CC-BY-SA</a>'})],
                 url: 'http://tile.openstreetmap.org/{z}/{x}/{y}.png'
             })
-        });var format_cartographieInssa_1 = new ol.format.GeoJSON();
-var features_cartographieInssa_1 = format_cartographieInssa_1.readFeatures(json_cartographieInssa_1, 
+        });var format_cartographieInssavf_1 = new ol.format.GeoJSON();
+var features_cartographieInssavf_1 = format_cartographieInssavf_1.readFeatures(json_cartographieInssavf_1, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_cartographieInssa_1 = new ol.source.Vector({
+var jsonSource_cartographieInssavf_1 = new ol.source.Vector({
     attributions: [new ol.Attribution({html: '<a href=""></a>'})],
 });
-jsonSource_cartographieInssa_1.addFeatures(features_cartographieInssa_1);cluster_cartographieInssa_1 = new ol.source.Cluster({
+jsonSource_cartographieInssavf_1.addFeatures(features_cartographieInssavf_1);cluster_cartographieInssavf_1 = new ol.source.Cluster({
   distance: 10,
-  source: jsonSource_cartographieInssa_1
-});var lyr_cartographieInssa_1 = new ol.layer.Vector({
+  source: jsonSource_cartographieInssavf_1
+});var lyr_cartographieInssavf_1 = new ol.layer.Vector({
                 declutter: true,
-                source:cluster_cartographieInssa_1, 
-                style: style_cartographieInssa_1,
-                title: '<img src="styles/legend/cartographieInssa_1.png" /> cartographieInssa'
+                source:cluster_cartographieInssavf_1, 
+                style: style_cartographieInssavf_1,
+                title: '<img src="styles/legend/cartographieInssavf_1.png" /> cartographieInssavf'
             });
 
-lyr_OSMStandard_0.setVisible(true);lyr_cartographieInssa_1.setVisible(true);
-var layersList = [lyr_OSMStandard_0,lyr_cartographieInssa_1];
-lyr_cartographieInssa_1.set('fieldAliases', {'Nom': 'Nom', 'Prénom': 'Prénom', 'Téléphone': 'Téléphone', 'E-mail': 'E-mail', 'Promotion': 'Promotion', 'Soutenance': 'Soutenance', 'Profil': 'Profil', 'Spécialité': 'Spécialité', 'Résidence': 'Résidence', 'X': 'X', 'Y': 'Y', });
-lyr_cartographieInssa_1.set('fieldImages', {'Nom': 'TextEdit', 'Prénom': 'TextEdit', 'Téléphone': 'TextEdit', 'E-mail': 'TextEdit', 'Promotion': 'TextEdit', 'Soutenance': 'TextEdit', 'Profil': 'TextEdit', 'Spécialité': 'TextEdit', 'Résidence': 'TextEdit', 'X': 'TextEdit', 'Y': 'TextEdit', });
-lyr_cartographieInssa_1.set('fieldLabels', {'Nom': 'header label', 'Prénom': 'header label', 'Téléphone': 'header label', 'E-mail': 'no label', 'Promotion': 'no label', 'Soutenance': 'no label', 'Profil': 'no label', 'Spécialité': 'no label', 'Résidence': 'no label', 'X': 'no label', 'Y': 'no label', });
-lyr_cartographieInssa_1.on('precompose', function(evt) {
+lyr_OSMStandard_0.setVisible(true);lyr_cartographieInssavf_1.setVisible(true);
+var layersList = [lyr_OSMStandard_0,lyr_cartographieInssavf_1];
+lyr_cartographieInssavf_1.set('fieldAliases', {'Nom': 'Nom', 'Prénom': 'Prénom', 'Téléphone': 'Téléphone', 'E-mail': 'E-mail', 'Promotion': 'Promotion', 'Soutenance': 'Soutenance', 'Profil': 'Profil', 'Spécialité': 'Spécialité', 'Structure': 'Structure', 'Y': 'Y', 'X': 'X', });
+lyr_cartographieInssavf_1.set('fieldImages', {'Nom': 'TextEdit', 'Prénom': 'TextEdit', 'Téléphone': 'TextEdit', 'E-mail': 'TextEdit', 'Promotion': 'TextEdit', 'Soutenance': 'TextEdit', 'Profil': 'TextEdit', 'Spécialité': 'TextEdit', 'Structure': 'TextEdit', 'Y': 'TextEdit', 'X': 'TextEdit', });
+lyr_cartographieInssavf_1.set('fieldLabels', {'Nom': 'inline label', 'Prénom': 'inline label', 'Téléphone': 'inline label', 'E-mail': 'inline label', 'Promotion': 'inline label', 'Soutenance': 'inline label', 'Profil': 'inline label', 'Spécialité': 'header label', 'Structure': 'inline label', 'Y': 'no label', 'X': 'no label', });
+lyr_cartographieInssavf_1.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
---
+++ styles/cartographieInssavf_1_style.js
@@ -0,0 +1,46 @@
+var size = 0;
+var placement = 'point';
+
+var style_cartographieInssavf_1 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = ""
+    var labelText = "";
+    var clusteredFeatures = feature.get("features");
+    var labelFont = "12.35px \'MS Shell Dlg 2\', sans-serif";
+    var labelFill = "rgba(10, 82, 238, 1)";
+    size = clusteredFeatures.length;
+    var textAlign = "center";
+    var offsetX = 0;
+    var offsetY = 0;
+    if (size == 1) {
+        textAlign = "left"
+        offsetX = 8
+        offsetY = 3
+        var feature = clusteredFeatures[0];
+        if (feature.get("Structure") !== null) {
+            labelText = String(feature.get("Structure"));
+        }
+        key = value + "_" + labelText
+    } else {
+        labelText = size.toString()
+        size = 2*(Math.log(size)/ Math.log(2))
+    }
+    var style = [ new ol.style.Style({
+        image: new ol.style.Icon({
+                  imgSize: [579.997, 580.457],
+                  scale: 0.0327587901317,
+                  anchor: [9, 9],
+                  anchorXUnits: "pixels",
+                  anchorYUnits: "pixels",
+                  rotation: 0.0,
+                  src: "styles/amenity=hospital.svg"
+            }),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+
+    return style;
+};
